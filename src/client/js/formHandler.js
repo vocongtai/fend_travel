d93e1dfeb10c015c1d6c01d1a8cbd846b12dd81d
TaiVC <TaiVoCong.1105@gmail.com>
2023-06-20 16:06:28 +0700

    v1.1

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -13,71 +13,77 @@ const handleSubmit = (event) => {
   //   Client.checkForName(formText);
   console.log(formText);
   console.log(dataPick);
-  console.log("::: Form Submitted :::");
-  getNameAPI("http://localhost:8081/geoname", { location: formText }).then(
-    (data) => {
-      console.log("data: ", data);
-      pixabayAPI("http://localhost:8081/pixabay", { place: data.city }).then(
-        (imageData) => {
-          imageCity = imageData;
-          console.log("imageCity: ", imageCity);
-        }
-      );
-      pixabayAPI("http://localhost:8081/pixabay", { place: data.country }).then(
-        (imageData) => {
+  if (dataPick === "") {
+    alert("Please Input date!!");
+  } else {
+    console.log("::: Form Submitted :::");
+    getNameAPI("http://localhost:8081/geoname", { location: formText }).then(
+      (data) => {
+        console.log("data: ", data);
+        pixabayAPI("http://localhost:8081/pixabay", { place: data.city }).then(
+          (imageData) => {
+            imageCity = imageData;
+            console.log("imageCity: ", imageCity);
+          }
+        );
+        pixabayAPI("http://localhost:8081/pixabay", {
+          place: data.country,
+        }).then((imageData) => {
           imageCountry = imageData;
           console.log("country: ", imageCountry);
-        }
-      );
-      weatherAPI("http://localhost:8081/weather", {
-        lat: data.lat,
-        long: data.long,
-        city: data.city,
-      }).then((weather) => {
-        // console.log("weather: ", weather);
-        for (const iterator of weather.data) {
-          if (dataPick !== "") {
-            if (iterator.datetime === dataPick) {
-              console.log(iterator);
-              //Update UI
-              resultForm.innerHTML = "";
-              const carddiv = document.createElement("div");
-              carddiv.setAttribute("class", "card");
-              const imgCityElement = document.createElement("img");
-              const imgCountryElement = document.createElement("img");
-              imgCityElement.src = imageCity;
-              imgCityElement.alt = "image Of city";
-              imgCountryElement.src = imageCountry;
-              imgCountryElement.alt = "image of country";
-              imgCityElement.style.width = "100%";
-              imgCountryElement.style.width = "100%";
-              const h4Element = document.createElement("h4");
-              h4Element.innerHTML = data.city;
-              h4Element.style.textAlign = "center";
-              const pWeather = document.createElement("p");
-              pWeather.innerHTML = `Weather: ${iterator.weather.description}`;
-              const pTemp = document.createElement("p");
-              pTemp.innerHTML = `Temp: ${iterator.temp}`;
-              const showMorebtn = document.createElement("button");
-              showMorebtn.innerHTML = "Show more of city";
-              carddiv.appendChild(imgCityElement);
-              carddiv.appendChild(h4Element);
-              carddiv.appendChild(pWeather);
-              carddiv.appendChild(pTemp);
-
-              carddiv.appendChild(showMorebtn);
-              showMorebtn.addEventListener("click", () => {
-                carddiv.appendChild(imgCountryElement);
-              });
-              resultForm.appendChild(carddiv);
+        });
+        weatherAPI("http://localhost:8081/weather", {
+          lat: data.lat,
+          long: data.long,
+          city: data.city,
+        }).then((weather) => {
+          // console.log("weather: ", weather);
+          for (const iterator of weather.data) {
+            if (dataPick !== "") {
+              if (iterator.datetime === dataPick) {
+                console.log(iterator);
+                //Update UI
+                resultForm.innerHTML = "";
+                const carddiv = document.createElement("div");
+                carddiv.setAttribute("class", "card");
+                const imgCityElement = document.createElement("img");
+                const imgCountryElement = document.createElement("img");
+                imgCityElement.src = imageCity;
+                imgCityElement.alt = "image Of city";
+                imgCountryElement.src = imageCountry;
+                imgCountryElement.alt = "image of country";
+                imgCityElement.style.width = "100%";
+                imgCountryElement.style.width = "100%";
+                const h4Element = document.createElement("h4");
+                h4Element.innerHTML = data.city;
+                h4Element.style.textAlign = "center";
+                const pWeather = document.createElement("p");
+                pWeather.innerHTML = `Weather: ${iterator.weather.description}`;
+                const pTemp = document.createElement("p");
+                pTemp.innerHTML = `Temp: ${iterator.temp}`;
+                const pDate = document.createElement("p");
+                pDate.innerHTML = `Date: ${iterator.datetime}`;
+                const showMorebtn = document.createElement("button");
+                showMorebtn.innerHTML = "Show more of city";
+                carddiv.appendChild(imgCityElement);
+                carddiv.appendChild(h4Element);
+                carddiv.appendChild(pWeather);
+                carddiv.appendChild(pTemp);
+                carddiv.appendChild(pDate);
+                carddiv.appendChild(showMorebtn);
+                showMorebtn.addEventListener("click", () => {
+                  carddiv.appendChild(imgCountryElement);
+                });
+                resultForm.appendChild(carddiv);
+              }
+            } else {
+              
             }
-          } else {
-            resultForm.innerHTML = "Please enter date!";
           }
-        }
-      });
-    }
-  );
+        });
+      }
+    );
+  }
 };
 
 /**
--- src/client/js/geoNameAPI.js
+++
@@ -1,8 +0,0 @@
-/**
- * GeoName fetch API
- */
-
-// const geoNameAPI = async (baseURL,location,apiKey)=>{
-//     const response = await fetch(`${baseURL}q=${location}&maxRows=10&username=${apiKey}`);
-
-// }
