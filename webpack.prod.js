2f712cb97af075d38c5cdb516dfd2d3d1530b4c8
TaiVC <TaiVoCong.1105@gmail.com>
2023-06-20 19:31:47 +0700

    v1.0

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -19,6 +20,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new WorkboxPlugin.GenerateSW(),
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
